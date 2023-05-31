088e6245c51238be3996d2016c0f5af7da0a66c2
martejazeke <117020858+martejazeke@users.noreply.github.com>
2023-06-01 01:19:22 +0400

    Add files via upload

---
+++ kfc/JavaScript.js
@@ -0,0 +1,6 @@
+function scrollToTop() {
+    window.scrollTo({
+    top: 0, /*allows to scroll to top of the page.*/
+    behavior: "smooth" /*makes the scrolling behavior smooth.*/
+    });
+    }
